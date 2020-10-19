6b05e185ce5b166e4948013236154a956dc85e42
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-10-19 12:35:41 -0700

    Add files via upload

---
+++ Encoder.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <unordered_map>
+ 
+using namespace std;
+const char alphabet[] = { ' ', '!', '"', '#', '$', '%', '&', '\'','(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', 'ˆ', '_', '`','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '}', '~' };
+int chunkSize = 2;
+string results="";
+string Encrypt(string secret) {
+    string str3;
+    unordered_map <char, int> map;
+    int alphaSize = sizeof(alphabet) / sizeof(alphabet[0]);
+    //Convert ASCII data to map
+    for (int i = 0; i < alphaSize; i++) {
+        map[alphabet[i]] = i;
+    }
+    //convert char into its hex equivalent
+    for (int i1 = 0; i1 < secret.length(); i1++) {
+        stringstream ss;
+        int i = map[secret[i1]];
+        ss << hex << i + 32; // int decimal_value
+        string res(ss.str());                
+        results+=res;
+    }
+
+    int stringLength = results.length();
+    //Now convert chunks of two ints into their unicode equivalent
+    for (int i6 = 0; i6 < stringLength; i6 += chunkSize)
+    {
+        if (i6 + chunkSize > stringLength) {
+            chunkSize = stringLength - i6;
+        }
+        string str2 = results.substr(i6, chunkSize);
+        stringstream ss2(str2);
+        int x = 0;
+        ss2 >> x;
+        str3.push_back(char(x));
+    }
+    return str3;
+}
+void main()
+{
+    string secrets;
+    stringstream holder;
+    ifstream sourceInput;
+    string out;
+    ofstream sourceOutput;
+    sourceInput.open("Input.txt");
+    sourceOutput.open("Output.txt");
+    while (getline(sourceInput, secrets))
+    {
+        results.clear();
+        holder.clear();
+        holder << Encrypt(secrets);
+        secrets = holder.str();
+    }
+    sourceOutput << secrets;
+    cout << secrets.length();
+    sourceOutput.close();
+    sourceInput.close();
+ 
+}
