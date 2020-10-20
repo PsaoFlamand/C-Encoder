660ef3ffa0ff7e6f9fd05289aa539b898a38278f
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-10-20 12:10:57 -0700

    Update Encoder.cpp

--- Encoder.cpp
+++ Encoder.cpp
@@ -7,16 +7,14 @@
 #include <cstdlib>
 using namespace std;
 
-int chunkSize = 2;
-string results = "";
 string Encrypt(string secret) {
     string str3;
     //convert char into its hex equivalent
     for (int i1 = 0; i1 < secret.length(); i1++) {
         stringstream ss;
         char a = secret[i1];
-        int ia = (int)a; ////Convert ASCII
-        ss << hex << a + 32; // Convert dec to hex
+        int ia = a; ////Convert ASCII
+        ss << hex << ia + 32; // Convert dec to hex
         string res(ss.str());
         wchar_t wc = strtol(res.c_str(), NULL, 16); //Convert to unicode     
         str3.push_back(wc);
@@ -34,14 +32,11 @@ void main()
     sourceOutput.open("Output.txt");
     while (getline(sourceInput, secrets))
     {
-        results.clear();
         holder.clear();
         holder << Encrypt(secrets);
         secrets = holder.str();
     }
     sourceOutput << secrets;
-    cout << secrets << endl;
-    cout << secrets.length();
     sourceOutput.close();
     sourceInput.close();
 
