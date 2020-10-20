05cc1c2bed0b34ebd420ceb47d60b48fbcce2852
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-10-20 15:11:36 -0700

    Update Encoder.cpp

--- Encoder.cpp
+++ Encoder.cpp
@@ -4,37 +4,59 @@
 #include <sstream>
 #include <stdio.h>
 #include <cstdlib>
+//Substitution Cipher
 using namespace std;
 
 string Encrypt(string secret) {
     string result;
-    //convert char into its hex equivalent
-    for (int i = 0; i < secret.length(); i++) {
-        stringstream ss;
-        char a = secret[i];
-        int ia = a; ////Convert ASCII
-        ss << hex << ia + 32; // Convert dec to hex
-        string res(ss.str());
-        wchar_t uni = strtol(res.c_str(), NULL, 16); //Convert to unicode     
-        result.push_back(uni);
+    char a;
+    for (int i = 0; i < secret.length(); i++) {//If odd, shift letter forward
+        if (i % 2 == 0) {
+            a = secret[i] + i;
+        }
+        else {//If odd, shift letter back
+            a = secret[i] - i;
+        }
+        result.push_back(a);
+    }
+    return result;
+}
+string Decrypt(string truth) {
+    string result;
+    char a;
+    for (int i = 0; i < truth.length(); i++) {
+        if (i % 2 == 0) { //If even, shift letter back
+            a = truth[i] - i;
+        }
+        else {//If odd, shift letter forward
+            a = truth[i] + i;
+        }
+        result.push_back(a);
     }
     return result;
 }
 void main()
 {
     string secrets;
-    stringstream holder;
+    string truth;
+    stringstream h1,h2;
     ifstream sourceInput;
     ofstream sourceOutput;
     sourceInput.open("Input.txt");
     sourceOutput.open("Output.txt");
     while (getline(sourceInput, secrets))
     {
-        holder.clear();
-        holder << Encrypt(secrets);
-        secrets = holder.str();
+     
+        h1 << Encrypt(secrets);
+        secrets = h1.str();
+        h2 << Decrypt(secrets);
+        truth = h2.str();
+
+
     }
     sourceOutput << secrets;
+    cout << secrets<<endl;
+    cout << truth;
     sourceOutput.close();
     sourceInput.close();
 
